11d729521af31f6ae3e6b913874d0b578c969884
FranciscoMendxza <101731715+FranciscoMendxza@users.noreply.github.com>
2023-11-15 11:36:58 -0600

    cuatro

--- components copy/mapa.php
+++
@@ -1,2 +0,0 @@
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470.43042718819186!2d-98.99738888120561!3d19.393238151723583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1e3a053dfb6c9%3A0x34205d6c6754ce47!2sBirrieria%20La%20Escondida!5e0!3m2!1ses!2smx!4v1698201871558!5m2!1ses!2smx" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
